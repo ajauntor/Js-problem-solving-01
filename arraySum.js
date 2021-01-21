cb015b77843e6072f50fe96871817813c69df02d
AJ Auntor <auntorknocks@gmail.com>
2021-01-22 01:05:18 +0600

    array sum

---
+++ arraySum.js
@@ -0,0 +1,12 @@
+
+function getAllSum(marks) {
+    var sum = 0;
+    for (var i = 0; i < marks.length; i++) {
+        var element = marks[i];
+        sum = sum + element;
+    }
+    return sum;
+}
+var marks = [63, 45, 87, 57, 14, 34, 50];
+var result= getAllSum(marks);
+console.log("totals of the number:", result);
