9635a41495e0622e660df5e1ce59746b5b97b3a0
AJ Auntor <auntorknocks@gmail.com>
2021-01-21 22:25:01 +0600

    Swap-problem

---
+++ README.md
@@ -0,0 +1,1 @@
+# Js-problem-solving-01
---
+++ swap.js
@@ -0,0 +1,7 @@
+var a = 10;
+var b = 17;
+console.log("before swap a=", a, "b=", b);
+var temp = a;
+a = b;
+b = temp;
+console.log("After swap a=", a, "b=", b);
