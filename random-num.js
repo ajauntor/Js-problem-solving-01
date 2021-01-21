38c89c5e126cdc5071ee3da4baa2ee72b04ed6e6
AJ Auntor <auntorknocks@gmail.com>
2021-01-21 22:54:25 +0600

    created random digit

--- random-num.js
+++ random-num.js
@@ -1,5 +1,11 @@
 var digit = 3.68777;
 var result = Math.ceil(digit);
-var result1=Math.round(digit);
-console.log(result);
-console.log(result1);
+var result1 = Math.round(digit);
+
+// console.log(result);
+// console.log(result1);
+
+var numberDice = Math.random() * 20;
+var numberFinalDice=Math.round(numberDice);
+console.log(numberFinalDice);
+
