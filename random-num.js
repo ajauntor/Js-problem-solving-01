04ff89c08e8e094a90cbab4083a8a86ffa4912da
AJ Auntor <auntorknocks@gmail.com>
2021-01-21 23:10:51 +0600

    math random with for loop

--- random-num.js
+++ random-num.js
@@ -4,8 +4,11 @@ var result1 = Math.round(digit);
 
 // console.log(result);
 // console.log(result1);
+for (var i = 0; i < 15; i++) {
+    var numberDice = Math.random() * 20;
+    var numberFinalDice = Math.round(numberDice);
+    console.log(numberFinalDice);
 
-var numberDice = Math.random() * 20;
-var numberFinalDice=Math.round(numberDice);
-console.log(numberFinalDice);
+}
+
 
