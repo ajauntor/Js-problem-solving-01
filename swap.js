583c389ddce58faa0c01bae5db44b449012d14a9
AJ Auntor <auntorknocks@gmail.com>
2021-02-21 18:17:25 +0600

    My Salary vs Samira's Salary Swap

--- swap.js
+++ swap.js
@@ -1,8 +1,14 @@
+// My Village vs My Mom Village Swap............!
 var ourVillage = "Hossenpur";
 var myMomVillage = "Donaitori"
-
 var villageChanger = myMomVillage;
 myMomVillage = ourVillage;
 ourVillage = villageChanger;
-
 console.log("Our village name is:", ourVillage, "My Mom village Name is:", myMomVillage);
+
+
+// My Total Salary vs Samira's Total Salary Swap....................!
+var myTotalSalary = 100;
+var samiraTotalSalary = 70;
+[myTotalSalary, samiraTotalSalary] = [samiraTotalSalary ,myTotalSalary];
+console.log("Auntor's Total Salary is:", myTotalSalary , "Samira's Total Salary is:", samiraTotalSalary);
