003639217781cf53f20b9c2c3ec9a4f0bb6c6c84
AJ Auntor <auntorknocks@gmail.com>
2021-02-26 19:33:24 +0600

    Amdaduls Salary is big there

--- array-max.js
+++ array-max.js
@@ -1,10 +1,6 @@
-var marks = [45, 81, 30, 97, 71, 94, 12];
-var max = marks[0];
-for (i = 0; i < marks.length; i++) {
-    var element = marks[i];
-    if (element > max) {
-        max = element;
-    }
-}
-
-console.log("Higest Value is =", max);
+// Find big Array............!
+var auntorSalary = 250;
+var salajSalary = 700;
+var amdadulSalary = 950;
+var findMax = Math.max(auntorSalary, salajSalary ,amdadulSalary);
+console.log(findMax);
